3850f829ea530b3c8cee6552e5af64503b24f763
Heather Anderson & Dexter Fitch <student@epicodus.com>
2015-02-09 10:02:59 -0800

    Fixed bug with display after numerous years run.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
   return false;
@@ -7,6 +7,7 @@ var leapYear = function(year) {
 };
 
 $(document).ready(function() {
+
   $("form#leap-year").submit(function(event) {
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
@@ -14,6 +15,8 @@ $(document).ready(function() {
     $(".year").text(year);
     if (!result) {
       $(".not").text("not");
+    } else {
+      $(".not").text("");
     }
 
     $("#result").show();
